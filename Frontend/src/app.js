1d63425c2cc9256f2f37398720cebc268ae89a78
huak <komsark_@hotmail.co.th>
2020-12-28 16:23:57 +0700

    commit

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -8,3 +8,16 @@
 
 
 // export default App;
+
+// ที่รวม Routh ต่างๆ เช่น
+
+// {/* <Route exact path="/" component={Home}/> 
+//         <Route path="/Addstudent" component={Addstudent}/>
+//         <Route path="/Addinstructor" component={Addinstructor}/>
+//         <Route path="/Addcourse" component={Addcourse}/>
+//         <Route path="/Addproject" component={Addproject}/>
+//         <Route path="/Login" component={Login}/> 
+//         <Route path="/evaluate/:project_id" component={Evaluate}/> 
+//         <Route path="/showproject" component={showproject}/>
+//         <Route path="/projectdetail/:project_id" component={projectdetail}/>
+//         <Route path="/score/:project_id" component={score}/> */}
