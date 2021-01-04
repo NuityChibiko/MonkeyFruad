4b69c7724f11f0ee042e5f18a089c707c19560b5
plunchyy <pu.punch@hotmail.com>
2021-01-04 15:11:01 +0700

    nav update

--- Frontend/src/user/components/navbar.js
+++ Frontend/src/user/components/navbar.js
@@ -7,7 +7,7 @@ const Usernvabar = () => {
   return (
     <div className="Navbar">
         <Navbar variant="dark" expand="lg">
-        <Navbar.Brand href="/">Monkey Fraud</Navbar.Brand>
+        <Navbar.Brand href="/" className="head">Monkey Fraud</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
