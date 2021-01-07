f9e5f4fc8bc6f89af7448fa010c8afd93bbb7b78
huak <komsark_@hotmail.co.th>
2021-01-07 19:08:52 +0700

    commit

--- Frontend/src/user/pages/signup.js
+++ Frontend/src/user/pages/signup.js
@@ -5,7 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 // import { Label, Input } from 'reactstrap';
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdbreact';
 import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
 const Signup = () => {
   return (
     <div>
@@ -138,6 +140,94 @@ const Signup = () => {
               </div>
             </div>
 
+            <div className="Province">
+              <label for="province" className="label-form-title">เขต</label>
+              <div className="form-inside ">
+                <Form.Group controlId="exampleForm.ControlSelect1">
+                  <Form.Control as="select">
+                  <option value="" selected>กรุณาเลือกเขต</option>
+                  <option value="กรุงเทพมหานคร">กรุงเทพมหานคร</option>
+                  <option value="กระบี่">กระบี่ </option>
+                  <option value="กาญจนบุรี">กาญจนบุรี </option>
+                  <option value="กาฬสินธุ์">กาฬสินธุ์ </option>
+                  <option value="กำแพงเพชร">กำแพงเพชร </option>
+                  <option value="ขอนแก่น">ขอนแก่น</option>
+                  <option value="จันทบุรี">จันทบุรี</option>
+                  <option value="ฉะเชิงเทรา">ฉะเชิงเทรา </option>
+                  <option value="ชัยนาท">ชัยนาท </option>
+                  <option value="ชัยภูมิ">ชัยภูมิ </option>
+                  <option value="ชุมพร">ชุมพร </option>
+                  <option value="ชลบุรี">ชลบุรี </option>
+                  <option value="เชียงใหม่">เชียงใหม่ </option>
+                  <option value="เชียงราย">เชียงราย </option>
+                  <option value="ตรัง">ตรัง </option>
+                  <option value="ตราด">ตราด </option>
+                  <option value="ตาก">ตาก </option>
+                  <option value="นครนายก">นครนายก </option>
+                  <option value="นครปฐม">นครปฐม </option>
+                  <option value="นครพนม">นครพนม </option>
+                  <option value="นครราชสีมา">นครราชสีมา </option>
+                  <option value="นครศรีธรรมราช">นครศรีธรรมราช </option>
+                  <option value="นครสวรรค์">นครสวรรค์ </option>
+                  <option value="นราธิวาส">นราธิวาส </option>
+                  <option value="น่าน">น่าน </option>
+                  <option value="นนทบุรี">นนทบุรี </option>
+                  <option value="บึงกาฬ">บึงกาฬ</option>
+                  <option value="บุรีรัมย์">บุรีรัมย์</option>
+                  <option value="ประจวบคีรีขันธ์">ประจวบคีรีขันธ์ </option>
+                  <option value="ปทุมธานี">ปทุมธานี </option>
+                  <option value="ปราจีนบุรี">ปราจีนบุรี </option>
+                  <option value="ปัตตานี">ปัตตานี </option>
+                  <option value="พะเยา">พะเยา </option>
+                  <option value="พระนครศรีอยุธยา">พระนครศรีอยุธยา </option>
+                  <option value="พังงา">พังงา </option>
+                  <option value="พิจิตร">พิจิตร </option>
+                  <option value="พิษณุโลก">พิษณุโลก </option>
+                  <option value="เพชรบุรี">เพชรบุรี </option>
+                  <option value="เพชรบูรณ์">เพชรบูรณ์ </option>
+                  <option value="แพร่">แพร่ </option>
+                  <option value="พัทลุง">พัทลุง </option>
+                  <option value="ภูเก็ต">ภูเก็ต </option>
+                  <option value="มหาสารคาม">มหาสารคาม </option>
+                  <option value="มุกดาหาร">มุกดาหาร </option>
+                  <option value="แม่ฮ่องสอน">แม่ฮ่องสอน </option>
+                  <option value="ยโสธร">ยโสธร </option>
+                  <option value="ยะลา">ยะลา </option>
+                  <option value="ร้อยเอ็ด">ร้อยเอ็ด </option>
+                  <option value="ระนอง">ระนอง </option>
+                  <option value="ระยอง">ระยอง </option>
+                  <option value="ราชบุรี">ราชบุรี</option>
+                  <option value="ลพบุรี">ลพบุรี </option>
+                  <option value="ลำปาง">ลำปาง </option>
+                  <option value="ลำพูน">ลำพูน </option>
+                  <option value="เลย">เลย </option>
+                  <option value="ศรีสะเกษ">ศรีสะเกษ</option>
+                  <option value="สกลนคร">สกลนคร</option>
+                  <option value="สงขลา">สงขลา </option>
+                  <option value="สมุทรสาคร">สมุทรสาคร </option>
+                  <option value="สมุทรปราการ">สมุทรปราการ </option>
+                  <option value="สมุทรสงคราม">สมุทรสงคราม </option>
+                  <option value="สระแก้ว">สระแก้ว </option>
+                  <option value="สระบุรี">สระบุรี </option>
+                  <option value="สิงห์บุรี">สิงห์บุรี </option>
+                  <option value="สุโขทัย">สุโขทัย </option>
+                  <option value="สุพรรณบุรี">สุพรรณบุรี </option>
+                  <option value="สุราษฎร์ธานี">สุราษฎร์ธานี </option>
+                  <option value="สุรินทร์">สุรินทร์ </option>
+                  <option value="สตูล">สตูล </option>
+                  <option value="หนองคาย">หนองคาย </option>
+                  <option value="หนองบัวลำภู">หนองบัวลำภู </option>
+                  <option value="อำนาจเจริญ">อำนาจเจริญ </option>
+                  <option value="อุดรธานี">อุดรธานี </option>
+                   <option value="อุตรดิตถ์">อุตรดิตถ์ </option>
+                  <option value="อุทัยธานี">อุทัยธานี </option>
+                  <option value="อุบลราชธานี">อุบลราชธานี</option>
+                  <option value="อ่างทอง">อ่างทอง </option>
+                  </Form.Control>
+                </Form.Group>
+              </div>
+            </div>
+            
             <div className="Email">
               <MDBInput  className="" label="Email" group type="email" validate />
             </div>
@@ -161,7 +251,7 @@ const Signup = () => {
           </button>
 
           <div className="Signup text-center pt-3" >
-            <span></span><a href="#">เข้าสู่ระบบ</a>
+            <span></span><a href="/login">เข้าสู่ระบบ</a>
             <hr></hr>
           </div>
 
