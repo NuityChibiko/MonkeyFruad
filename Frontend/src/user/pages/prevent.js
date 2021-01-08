b289629a3412fde20677cc629841180191673ced
Porpraew <praew15888@hotmail.com>
2021-01-08 21:30:06 +0700

    555

--- Frontend/src/user/pages/prevent.js
+++ Frontend/src/user/pages/prevent.js
@@ -1,76 +1,52 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import Chatbot from "../components/chatbot";
 import "./prevent.css";
+import {Image} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const Prevent = () => {
   return (
     <div>
       <Navbar />
       <h1>รู้ไว้ไม่โดนโกง</h1>
       <div className="container-prevent">
-        <div className="row">
-          <div className="column">
-            <img src="/img/prevent1.png" className="image" />
-            <p className="text">
-              ซื้อสินค้าออนไลน์แล้ว
-              <br />
-              ไม่ได้รับสินค้าตามกำหนด
-            </p>
+          <div className="a1 a1-left">
+            <img src="/image/prevent1.png" className="image1" />
+            <p className="text-content">ซื้อสินค้าออนไลน์แล้ว<br className="br"/>ไม่ได้รับสินค้าตามกำหนด</p>
           </div>
-          <div className="column">
-            <img src="/img/prevent2.png" className="image" />
-            <p className="text">
-              ล่อลวงให้โอนเงินค่าสินค้าล่วงหน้า
-              <br />
-              โดยไม่มีหลักฐานที่น่าเชื่อถือเพียงพอ
-            </p>
+          <div className="a1 a1-right">
+            <img src="/image/prevent2.png" className="image1" />
+            <p className="text-content">ล่อลวงให้โอนเงินค่าสินค้าล่วงหน้า<br className="br"/>โดยไม่มีหลักฐานที่น่าเชื่อถือเพียงพอ</p>
           </div>
-          <div className="column">
-            <img src="/img/prevent3.png" className="image" />
-            <p className="text">
-              เมื่อโอนเงินแล้วผู้ขายจะหายตัวไป
-              <br />
-              ปิดช่องทางการติดต่อทุกทาง
-            </p>
+          <div className="a1">
+            <img src="/image/prevent3.png" className="image1" />
+            <p className="text-content">เมื่อโอนเงินแล้วผู้ขายจะหายตัวไป<br className="br"/>ปิดช่องทางการติดต่อทุกทาง<p className="text-none textwhite">a<br/></p></p>
           </div>
-        </div>
-        <div className="row">
-          <div className="column a">
-            <img src="/img/prevent6.png" className="image" />
-            <p className="text">
-              ปลอม SMS จากธนาคารเพื่อ
-              <br />
-              หลอกโอนเงิน หรือหลอกว่าให้
-              <br />
-              โอนเงินคืน เพราะโอนเงินเกิน
-            </p>
+          <div className="b1 b1-left">
+            <img src="/image/prevent6.png" className="image1" />
+            <p className="text-content">ปลอม SMS จากธนาคารเพื่อ<br className="br"/>หลอกโอนเงิน หรือหลอกว่าให้<br className="br"/>โอนเงินคืน เพราะโอนเงินเกิน</p>
           </div>
-          <div className="column b">
-            <img src="/img/prevent7.png" className="image" />
-            <p className="text">
-              อ้างว่า เว็บซื้อขายสินค้า เป็นคนกลาง
-              <br />
-              ในการซื้อขายสินค้า ให้เหยื่อติดต่อ
-              <br />
-              รับเงินจากคืนเว็บไซต์เอง
-            </p>
+          <div className="b1 b1-right">
+            <img src="/image/prevent7.png" className="image1" />
+            <p className="text-content">อ้างว่า เว็บซื้อขายสินค้า เป็นคนกลาง<br className="br"/>ในการซื้อขายสินค้า ให้เหยื่อติดต่อ<br className="br"/>รับเงินจากคืนเว็บไซต์เอง</p>
+          </div>
+          <div className="a1">
+            <img src="/image/prevent4.png" className="image1" />
+            <p className="text-content">เปลี่ยนชื่อและเบอร์โทรฯ <br className="text-none2"/> ไม่ซ้ำกัน<p className="text-none textwhite"><br/>a<br/><br className="text-none3"/></p></p>
+          </div>
+          <div className="a1">
+            <img src="/image/prevent5.png" className="image1" />
+            <p className="text-content">นัดเจอเพื่อรับของก่อนแล้วโอนทีหลัง</p>
+          </div>
+          <div className="a1">
+            <img src="/image/prevent8.png" className="image1" />
+            <p className="text-content">ขายสินค้าถูกกว่าท้องตลาดเกินไป</p>
           </div>
         </div>
-        <div className="row">
-          <div className="column">
-            <img src="/img/prevent4.png" className="image" />
-            <p className="text">เปลี่ยนชื่อและเบอร์โทรฯ ไม่ซ้ำกัน</p>
-          </div>
-          <div className="column">
-            <img src="/img/prevent5.png" className="image" />
-            <p className="text">นัดเจอเพื่อรับของก่อนแล้วโอนทีหลัง</p>
-          </div>
-          <div className="column">
-            <img src="/img/prevent8.png" className="image" />
-            <p className="text">ขายสินค้าถูกกว่าท้องตลาดเกินไป</p>
-          </div>
-        </div>
+       <Chatbot/>
       </div>
-    </div>
+      
+
   );
 };
 
