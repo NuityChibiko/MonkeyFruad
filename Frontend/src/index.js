2e721e74c28480ad0a3bb73a03962cd1f45da159
Bawornsak Luachan <59905945+Bawornsak@users.noreply.github.com>
2021-01-09 17:29:04 +0700

    commit

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
 
-=======
 import '@fortawesome/fontawesome-free/css/all.min.css'; 
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
->>>>>>> 46f09d1ec375f12b353846fdd8c963438c6fcd9e
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- Frontend/src/user/pages/mypost.js
+++ Frontend/src/user/pages/mypost.js
@@ -22,163 +22,163 @@
 //     const [other, setOther] = useState();
     
 
-  return (
-    <div className="allpage">
-      <Navbar />
-      <h1>โพสต์ของฉัน</h1>
-        <div className="container-mypost">
-            <div className="cotainer-mypost2">
-                <div className="profile-img">
-                    <img className="img-circle" src="/img/profile.png" />
-                        <div className="name">
-                            @Nuitychibiko
-                        </div><br/>
-                        <div className="date">
-                            13/11/2563 
-                            <span className="time">23:38 </span>
-                        </div>
-                </div>
+//   return (
+//     <div className="allpage">
+//       <Navbar />
+//       <h1>โพสต์ของฉัน</h1>
+//         <div className="container-mypost">
+//             <div className="cotainer-mypost2">
+//                 <div className="profile-img">
+//                     <img className="img-circle" src="/img/profile.png" />
+//                         <div className="name">
+//                             @Nuitychibiko
+//                         </div><br/>
+//                         <div className="date">
+//                             13/11/2563 
+//                             <span className="time">23:38 </span>
+//                         </div>
+//                 </div>
 
-                <div className="buttonshared">
-                    <a className="buttonshare"
-                        href="/post/edit">
-                        <i class="fas fa-share"></i>
-                    </a>
-                </div>
+//                 <div className="buttonshared">
+//                     <a className="buttonshare"
+//                         href="/post/edit">
+//                         <i class="fas fa-share"></i>
+//                     </a>
+//                 </div>
 
-                <div className="container-setiing">
-                    <div className="menu-containersetting">
-                        <div onClick={onClick} className="buttonsetting">
-                            <img className="img-setting"
-                                src="/setting.png"
-                                alt="User avatar">
-                            </img>
-                        </div>
-                        <div
-                            className={`menusetting ${isActive ? "active" : "inactive"}`}>
-                            <ul>
-                                <li>
-                                    <a href="/post/edit">แก้ไขโพสต์</a>
-                                </li>
-                                <li>
-                                    <a href="#">ลบโพสต์</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+//                 <div className="container-setiing">
+//                     <div className="menu-containersetting">
+//                         <div onClick={onClick} className="buttonsetting">
+//                             <img className="img-setting"
+//                                 src="/setting.png"
+//                                 alt="User avatar">
+//                             </img>
+//                         </div>
+//                         <div
+//                             className={`menusetting ${isActive ? "active" : "inactive"}`}>
+//                             <ul>
+//                                 <li>
+//                                     <a href="/post/edit">แก้ไขโพสต์</a>
+//                                 </li>
+//                                 <li>
+//                                     <a href="#">ลบโพสต์</a>
+//                                 </li>
+//                             </ul>
+//                         </div>
+//                     </div>
+//                 </div>
 
-                <div className="container-mypost3">
-                    <div className="profile-bad-img">
-                        <img className="img-circle" src="/img/profile.png" />
-                    </div>
-                    <Form className="formsize">
-                        <Form.Row>
-                            <Form.Group
-                                as={Col}
-                                className="left col-lg-6 col-12"
-                                controlId="formGridName"
-                                >
-                                <Form.Label>
-                                    ชื่อ (ผู้โกง) <span>xxxxxxxx</span>
-                                </Form.Label>
-                            </Form.Group>
+//                 <div className="container-mypost3">
+//                     <div className="profile-bad-img">
+//                         <img className="img-circle" src="/img/profile.png" />
+//                     </div>
+//                     <Form className="formsize">
+//                         <Form.Row>
+//                             <Form.Group
+//                                 as={Col}
+//                                 className="left col-lg-6 col-12"
+//                                 controlId="formGridName"
+//                                 >
+//                                 <Form.Label>
+//                                     ชื่อ (ผู้โกง) <span>xxxxxxxx</span>
+//                                 </Form.Label>
+//                             </Form.Group>
 
-                            <Form.Group as={Col} controlId="formGridLastname">
-                                <Form.Label>
-                                    นามสกุล (ผู้โกง) <span>xxxxxxxx</span>
-                                </Form.Label>
-                            </Form.Group>
-                        </Form.Row>
+//                             <Form.Group as={Col} controlId="formGridLastname">
+//                                 <Form.Label>
+//                                     นามสกุล (ผู้โกง) <span>xxxxxxxx</span>
+//                                 </Form.Label>
+//                             </Form.Group>
+//                         </Form.Row>
 
-                        <Form.Row>
-                            <Form.Group
-                                as={Col}
-                                className="left col-lg-6 col-12"
-                                controlId="formGridId"
-                                >
-                                <Form.Label>
-                                    เลขบัตรประชาชน (ผู้โกง) <span>xxxxxxxx</span>
-                                </Form.Label>
-                            </Form.Group>
+//                         <Form.Row>
+//                             <Form.Group
+//                                 as={Col}
+//                                 className="left col-lg-6 col-12"
+//                                 controlId="formGridId"
+//                                 >
+//                                 <Form.Label>
+//                                     เลขบัตรประชาชน (ผู้โกง) <span>xxxxxxxx</span>
+//                                 </Form.Label>
+//                             </Form.Group>
 
-                            <Form.Group as={Col} controlId="formGridAccountnumber">
-                                <Form.Label>
-                                    เลขที่บัญชี (ผู้โกง) <span>xxxxxxxx</span>
-                                </Form.Label>
-                            </Form.Group>
-                        </Form.Row>
+//                             <Form.Group as={Col} controlId="formGridAccountnumber">
+//                                 <Form.Label>
+//                                     เลขที่บัญชี (ผู้โกง) <span>xxxxxxxx</span>
+//                                 </Form.Label>
+//                             </Form.Group>
+//                         </Form.Row>
 
-                        <Form.Row>
-                            <Form.Group
-                                as={Col}
-                                className="left col-lg-6 col-12"
-                                controlId="formGridNameproduct"
-                                >
-                                <Form.Label>
-                                    ชื่อสินค้า <span>xxxxxxxx</span>
-                                </Form.Label>
-                            </Form.Group>
+//                         <Form.Row>
+//                             <Form.Group
+//                                 as={Col}
+//                                 className="left col-lg-6 col-12"
+//                                 controlId="formGridNameproduct"
+//                                 >
+//                                 <Form.Label>
+//                                     ชื่อสินค้า <span>xxxxxxxx</span>
+//                                 </Form.Label>
+//                             </Form.Group>
 
-                            <Form.Group as={Col} controlId="formGridCategory">
-                                <Form.Label>
-                                    หมวดหมู่สินค้า <span>xxxxxxxx</span>
-                                </Form.Label>
-                            </Form.Group>
-                        </Form.Row>
+//                             <Form.Group as={Col} controlId="formGridCategory">
+//                                 <Form.Label>
+//                                     หมวดหมู่สินค้า <span>xxxxxxxx</span>
+//                                 </Form.Label>
+//                             </Form.Group>
+//                         </Form.Row>
 
-                        <Form.Row>
-                            <Form.Group
-                                as={Col}
-                                className="left col-lg-6 col-12"
-                                controlId="formGridPrice"
-                                >
-                                <Form.Label>
-                                    จำนวนเงิน (บาท) <span>xxxxxxxx</span>
-                                </Form.Label>
-                            </Form.Group>
+//                         <Form.Row>
+//                             <Form.Group
+//                                 as={Col}
+//                                 className="left col-lg-6 col-12"
+//                                 controlId="formGridPrice"
+//                                 >
+//                                 <Form.Label>
+//                                     จำนวนเงิน (บาท) <span>xxxxxxxx</span>
+//                                 </Form.Label>
+//                             </Form.Group>
 
-                            <Form.Group as={Col} controlId="formGridCategory">
-                                <Form.Label>
-                                    ธนาคาร <span>xxxxxxxx</span>
-                                </Form.Label>
-                            </Form.Group>
-                        </Form.Row>
+//                             <Form.Group as={Col} controlId="formGridCategory">
+//                                 <Form.Label>
+//                                     ธนาคาร <span>xxxxxxxx</span>
+//                                 </Form.Label>
+//                             </Form.Group>
+//                         </Form.Row>
 
-                        <Form.Row>
-                            <Form.Group
-                                as={Col}
-                                className="left col-lg-6 col-12"
-                                controlId="formGridDate"
-                                >
-                                <Form.Label>
-                                    วันที่โดนโกง <span>xxxxxxxx</span>
-                                </Form.Label>
-                            </Form.Group>
+//                         <Form.Row>
+//                             <Form.Group
+//                                 as={Col}
+//                                 className="left col-lg-6 col-12"
+//                                 controlId="formGridDate"
+//                                 >
+//                                 <Form.Label>
+//                                     วันที่โดนโกง <span>xxxxxxxx</span>
+//                                 </Form.Label>
+//                             </Form.Group>
 
-                            <Form.Group as={Col} controlId="formGridSocial">
-                                <Form.Label>
-                                    ช่องทางที่โดนโกง <span>xxxxxxxx</span>
-                                </Form.Label>
-                            </Form.Group>
-                        </Form.Row>
+//                             <Form.Group as={Col} controlId="formGridSocial">
+//                                 <Form.Label>
+//                                     ช่องทางที่โดนโกง <span>xxxxxxxx</span>
+//                                 </Form.Label>
+//                             </Form.Group>
+//                         </Form.Row>
 
-                        <Form.Group controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>
-                                รายละเอียดเพิ่มเติม <span>xxxxxxxx</span>
-                            </Form.Label>
-                        </Form.Group>
-                        <div className="img-holder-bad">
-                            <img className="img-bad"src="/img//nui.jpg" />
-                            <img className="img-bad"src="/img//nui.jpg" />
-                            <img className="img-bad"src="/img//nui.jpg" />
-                        </div>
-                    </Form>
-                </div>
-            </div>
-        </div>
-    </div>
-  );
-};
+//                         <Form.Group controlId="exampleForm.ControlTextarea1">
+//                             <Form.Label>
+//                                 รายละเอียดเพิ่มเติม <span>xxxxxxxx</span>
+//                             </Form.Label>
+//                         </Form.Group>
+//                         <div className="img-holder-bad">
+//                             <img className="img-bad"src="/img//nui.jpg" />
+//                             <img className="img-bad"src="/img//nui.jpg" />
+//                             <img className="img-bad"src="/img//nui.jpg" />
+//                         </div>
+//                     </Form>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//   );
+// };
 
-export default Mypost;
+// export default Mypost;
