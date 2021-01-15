0d9b5b900415655ab236955e16ccbc864e4d428e
huak <komsark_@hotmail.co.th>
2021-01-15 10:43:54 +0700

    commit

--- Frontend/src/user/pages/login.js
+++ Frontend/src/user/pages/login.js
@@ -70,7 +70,7 @@ const Login = () => {
         <form className="LoginForm">
           <img src="/img/logoLogin.png" className="LogoLogin" />
           <p className="h2 text-center mb-2 font-weight-bold">เข้าสู่ระบบ</p>
-          {emailis_inVaild ? <div className="alert-login"> <span>อีเมลนี้ไม่มีอยู่ในระบบ</span></div> : <p></p>}
+          {emailis_inVaild ? <div className="alert-login"> <span>อีเมลหรือรหัสผ่านไม่ถูกต้อง</span></div> : <p></p>}
           <div className="LoginInputForm">
             <MDBInput
               className="InputEmail"
