88f6bc60d1226239e04fe964ff304fceedae3e0f
Bawornsak Luachan <59905945+Bawornsak@users.noreply.github.com>
2021-01-17 13:07:58 +0700

    commit

---
+++ Frontend/src/user/components/MenuItems_help.js
@@ -0,0 +1,13 @@
+export const MenuItems_help = [
+    {
+      title: 'Marketing',
+      path: '/marketing',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'Consulting',
+      path: '/consulting',
+      cName: 'dropdown-link'
+    }
+  ];
+  
---
+++ Frontend/src/user/components/MenuItems_post.js
@@ -0,0 +1,13 @@
+export const MenuItems_post = [
+    {
+      title: 'โพสต์ทั้งหมด',
+      path: '/post',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'สร้างโพสต์',
+      path: '/linkruleshow',
+      cName: 'dropdown-link'
+    }
+  ];
+  
